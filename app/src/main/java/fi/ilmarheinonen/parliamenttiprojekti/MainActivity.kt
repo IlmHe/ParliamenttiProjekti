136b48eeab4b9ff95b3b6009b2f3594f06b7a20b
ilmar <ramli.flodur.nenonieh@gmail.com>
2022-03-01 16:08:10 +0200

    unversioned files

---
+++ app/src/main/java/fi/ilmarheinonen/parliamenttiprojekti/MainActivity.kt
@@ -0,0 +1,23 @@
+package fi.ilmarheinonen.parliamenttiprojekti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+    //https://proandroiddev.com/moshi-with-retrofit-in-kotlin-%EF%B8%8F-a69c2621708b
+}
